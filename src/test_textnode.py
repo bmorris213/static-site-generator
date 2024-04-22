162d1e6a6511937ded48bb34d126a84b88d4cd88
bmorris213 <thisisnotalreadyused@gmail.com>
2024-04-22 04:20:56 -0600

    Adding text types

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,13 +1,15 @@
 import unittest
 
 from textnode import TextNode
-
+from textnode import TextType
 
 class TestTextNode(unittest.TestCase):
-    def test_eq(self):
-        node = TextNode("This is a text node", "bold")
-        node2 = TextNode("This is a text node", "bold")
-        self.assertEqual(node, node2)
+    def test_text_to_html(self):
+        node = TextNode("This is a text node", TextType.bold)
+        node2 = TextNode("This is a text node", TextType.italic)
+        leaf = node.to_html()
+        print(leaf)
+        print(node2.to_html())
 
 
 if __name__ == "__main__":
--- src/textnode.py
+++ src/textnode.py
@@ -1,6 +1,20 @@
+from leafnode import LeafNode
+
+from enum import Enum
+
+class TextType(Enum):
+    text = 0
+    bold = 1
+    italic = 2
+    code = 3
+    link = 4
+    image = 5
+
 class TextNode:
     def __init__(self, text, text_type, url=None):
         self.text = text
+        if text_type not in list(TextType):
+            raise Exception("text type enum of an invalid type")
         self.text_type = text_type
         self.url = url
     
@@ -11,3 +25,19 @@ class TextNode:
     
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type}, {self.url})"
+    
+    def to_html(self):
+        if self.text_type == TextType.text:
+            return LeafNode(None, self.text)
+        elif self.text_type == TextType.bold:
+            return LeafNode("b", self.text)
+        elif self.text_type == TextType.italic:
+            return LeafNode("i", self.text)
+        elif self.text_type == TextType.code:
+            return LeafNode("code", self.text)
+        elif self.text_type == TextType.link:
+            return LeafNode("a", self.text, {"href":self.url})
+        elif self.text_type == TextType.image:
+            return LeafNode("img", "", {"src":self.url, "alt":self.text})
+        else:
+            raise Exception(f"text type ({self.text_type}) of text node is invalid")
