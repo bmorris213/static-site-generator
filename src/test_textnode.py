a01e7c11ec9d7d6f944ad1f8c27ee6e162c6e304
bmorris213 <thisisnotalreadyused@gmail.com>
2024-04-22 08:04:34 -0600

    adding delimiter seperator function, fixed some stuff with said delimiter function

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,16 +1,13 @@
 import unittest
 
 from textnode import TextNode
-from textnode import TextType
 
 class TestTextNode(unittest.TestCase):
     def test_text_to_html(self):
-        node = TextNode("This is a text node", TextType.bold)
-        node2 = TextNode("This is a text node", TextType.italic)
-        leaf = node.to_html()
-        print(leaf)
-        print(node2.to_html())
-
+        node = TextNode("This is a **text** node with **bold text** and *italic* text **ending** without being **bold", "bold")
+        splitnodes = node.split_delimiter("bold") # fix t_type vs delimiter class types lmao it took away the italic *s
+        for item in splitnodes:
+            print(item.to_html())
 
 if __name__ == "__main__":
     unittest.main()
--- src/textnode.py
+++ src/textnode.py
@@ -2,19 +2,16 @@ from leafnode import LeafNode
 
 from enum import Enum
 
-class TextType(Enum):
-    text = 0
-    bold = 1
-    italic = 2
-    code = 3
-    link = 4
-    image = 5
-
 class TextNode:
+    text_type_delimiters = {
+        "text" : "",
+        "bold" : "**",
+        "italic" : "*",
+        "code": "`"
+    }
+    
     def __init__(self, text, text_type, url=None):
         self.text = text
-        if text_type not in list(TextType):
-            raise Exception("text type enum of an invalid type")
         self.text_type = text_type
         self.url = url
     
@@ -26,18 +23,54 @@ class TextNode:
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type}, {self.url})"
     
+    def split_delimiter(self, t_type):
+        if t_type not in self.text_type_delimiters.keys():
+            # trying to split up using a type that has no delimiter
+            return list(self)
+        delimiter = self.text_type_delimiters[t_type]
+        result = []
+        temp = ""
+        foundword = ""
+        inside_delimiter = False
+        delimiter_length = len(delimiter)
+        current_index = 0
+
+        while current_index < len(self.text):
+            if self.text[current_index:current_index + delimiter_length] == delimiter:
+                inside_delimiter = not inside_delimiter
+                current_index += delimiter_length
+                if not inside_delimiter:
+                    result.append(TextNode(temp, "text"))
+                    result.append(TextNode(foundword, t_type))
+                    temp = ""
+                    foundword = ""
+                continue
+            elif self.text[current_index] != delimiter[0]:
+                if inside_delimiter:
+                    foundword += self.text[current_index]
+                else:
+                    temp += self.text[current_index]
+            current_index += 1
+        
+        if temp:
+            result.append(TextNode(temp, "text"))
+        if foundword:
+            # end of word found without a closing delimiter
+            result.append(TextNode(f"{delimiter}{foundword}", "text"))
+        return result
+
     def to_html(self):
-        if self.text_type == TextType.text:
+        if self.text_type == "text":
             return LeafNode(None, self.text)
-        elif self.text_type == TextType.bold:
+        elif self.text_type == "bold":
             return LeafNode("b", self.text)
-        elif self.text_type == TextType.italic:
+        elif self.text_type == "italic":
             return LeafNode("i", self.text)
-        elif self.text_type == TextType.code:
+        elif self.text_type == "code":
             return LeafNode("code", self.text)
-        elif self.text_type == TextType.link:
+        elif self.text_type == "link":
             return LeafNode("a", self.text, {"href":self.url})
-        elif self.text_type == TextType.image:
+        elif self.text_type == "image":
             return LeafNode("img", "", {"src":self.url, "alt":self.text})
         else:
             raise Exception(f"text type ({self.text_type}) of text node is invalid")
